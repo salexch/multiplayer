9ddc593ba681df71f76b98b99d2ffb16a3933551
Alex Chertin <alex@opentech.co.il>
2016-04-14 09:11:02 +0300

    fix play at index

--- dist/player.js
+++ dist/player.js
@@ -242,7 +242,7 @@
 	            return false;
 	        }
 	
-	        var init_playlist = this._playlist.slice(this._playlist_index, 2);
+	        var init_playlist = this._playlist.slice(this._playlist_index, this._playlist_index + 2);
 	
 	        var players_dfd = init_playlist.map(function(video) {
 	            var player_elem = _createPlayerElem();
